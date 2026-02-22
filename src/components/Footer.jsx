c88e0f45d1f008e01c023b96d477c8fa566eae16
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-02-22 07:28:15 +0100

    feat(footer): add initial Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+      <section className="bg-green-700 flex justify-between">
+        <div className="">
+          <h2 className="p-10">Company Info</h2>
+          <p className="p-10">About Futro</p>
+        </div>
+        <div>
+          <h2 className="p-10">Customer Service</h2>
+          <p className="p-10">Return Policy</p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Footer;
