7d9dc836cee85217836178f5ecde2ab64c826f0c
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-02-22 07:27:00 +0100

    feat(main): create semantic Main component structure for homepage

---
+++ src/components/Main.jsx
@@ -0,0 +1,25 @@
+function Main() {
+  return (
+    <>
+    <section className="bg-green-600 text-center">
+      <div className="p-20">
+        <h2>
+          Trending Campus Essentials
+        </h2>
+      </div>
+      <div className="m-80 p-20">
+        <h2>
+          Study & Hostel Accessories
+        </h2>
+      </div>
+      <div className="m-80 p-20" >
+        <h2>
+          Welcome to the Official Stores
+        </h2>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Main;
